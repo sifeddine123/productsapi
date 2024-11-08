4991713fe3825f98b4607c13698d3f5eb8804c41
Cherkaoui El Wafi <elwafi.Cherkaoui@gmail.com>
2024-11-08 21:29:59 +0100

    Fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [products, setProducts] = useState(0)
+  useEffect(()=>{
+    fetch('https://api.escuelajs.co/api/v1/products')
+    .then((res)=>res.json())
+    .then((data)=>{setProducts(data)})
+  },[])
+  console.log(products[0]);
+  
   return (
     <>
-      
+      <App products={products} />
     </>
   )
 }
