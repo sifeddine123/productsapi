a431eda8818c1f422e12a0448d7f0500377c3d7b
sif eddine <sifeddineafram@gmail.com>
2024-11-08 21:42:18 +0100

    'affichage'

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App({products}) {
+  
   return (
     <>
-      
+      {
+        products.map(function(product,i){
+          return (
+            <div key={i}>
+              <p>{product.id}</p>
+              <p>{product.title}</p>
+              <p>{product.price}</p>
+              <p>{product.description}</p>
+            </div>
+          )
+        })
+      }    
     </>
   )
 }
